108192126fbfb71c9ef3cda5654d0b666a058835
mrm28 <murilolion10@gmail.com>
2024-04-17 12:00:06 -0300

    teste

---
+++ template_javascript/index.js
@@ -0,0 +1,99 @@
+//DECLARAÇAO DAS VARIAVEIS
+let nome 
+let idade 
+let anoNascimento
+let anoAtual
+let maiorIdade
+let idade2050
+
+//RECEBE VALORES
+nome = prompt("Digite o seu nome")
+anoNascimento = prompt("Digite a data do seu nascimento")
+anoAtual = prompt("Digite o ano atual")
+
+//CALCULA OS VALORES PEDIDOS
+idade = anoAtual - anoNascimento
+maiorIdade = idade >= 18 
+idade2050 = 2050 - anoNascimento
+
+//RESULTADO NA TELA 
+alert(nome)
+alert(idade)
+alert(maiorIdade)
+alert(idade2050)
+
+let idade
+let idadeAmigo
+let idadeMaior
+let diferenca
+
+idade = prompt("Digite a dua idade:")
+idadeAmigoo = prompt("Digite a idade do seu amigo:")
+idadeMaior = idade >= idadeAmigo
+
+alert(idade)
+alert(idadeAmigo)
+
+
+let num1  = Number(prompt("Digite um numero:"))
+console.log(num2 % 2)
+
+let numImpar  = Number(prompt("Digite um numero:"))
+console.log(numImpar % 2)
+
+alert(num1)
+alert(numImpar)
+
+let idadeAnos = prompt("Digite  a sua idade:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
