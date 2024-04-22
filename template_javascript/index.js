95de71b17d979d20be2e70fbc208350cfc0bb597
mrm28 <murilolion10@gmail.com>
2024-04-22 11:20:11 -0300

    ENVIO

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -1,99 +1,13 @@
-//DECLARAÇAO DAS VARIAVEIS
-let nome 
-let idade 
-let anoNascimento
-let anoAtual
-let maiorIdade
-let idade2050
-
-//RECEBE VALORES
-nome = prompt("Digite o seu nome")
-anoNascimento = prompt("Digite a data do seu nascimento")
-anoAtual = prompt("Digite o ano atual")
-
-//CALCULA OS VALORES PEDIDOS
-idade = anoAtual - anoNascimento
-maiorIdade = idade >= 18 
-idade2050 = 2050 - anoNascimento
-
-//RESULTADO NA TELA 
-alert(nome)
-alert(idade)
-alert(maiorIdade)
-alert(idade2050)
-
-let idade
-let idadeAmigo
-let idadeMaior
-let diferenca
-
-idade = prompt("Digite a dua idade:")
-idadeAmigoo = prompt("Digite a idade do seu amigo:")
-idadeMaior = idade >= idadeAmigo
-
-alert(idade)
-alert(idadeAmigo)
-
-
-let num1  = Number(prompt("Digite um numero:"))
-console.log(num2 % 2)
-
-let numImpar  = Number(prompt("Digite um numero:"))
-console.log(numImpar % 2)
-
-alert(num1)
-alert(numImpar)
-
-let idadeAnos = prompt("Digite  a sua idade:")
+/*let comidaPreferida = ["Pizza","Arroz","Sushi","Feijao"]
+console.log(comidaPreferida[0] + "\n" + comidaPreferida[1]+ "\n" + comidaPreferida[2] + "\n + comidaPreferida[3]*/
 
 
 
 
 
 
+let nomeDoUsuario = prompt("Digite seu nome:")
 
+let emailDoUsuario = prompt("Digite seu E-mail:")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+alert(`Meu nome é ${nomeDoUsuario} e meu e-mail é ${emailDoUsuario}')
