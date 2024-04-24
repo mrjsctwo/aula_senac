48cd18f2c424f966e046f59d19c0f88435c4dd78
mrm28 <murilolion10@gmail.com>
2024-04-24 11:58:06 -0300

    Teste

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -247,3 +247,31 @@ while (i != 0) {
 //MOSTRA O RESILTADO NA TELA APÓS SAIR O LOOP
 alert(soma)
 */
+
+
+//----------------------------------------
+
+//--------FOR------------
+
+
+/*const numeros = [11, 15, 18, 14, 13]
+
+for(let i = 0; i < 5; i++){
+    const elemento = numeros[i]
+    console.log(elemento)
+
+}
+*/
+
+const numeros = [11, 15, 18, 14, 12, 13]
+function devolveMaiorNumero(array) {
+ let maiorNumero = 0
+ for(let i = 0; i < array.length; i++) {
+ let numeroAtual = array[i]
+ if(numeroAtual >= maiorNumero) {
+ maiorNumero = numeroAtual
+ }
+ }
+ console.log(`O maior número do array é ${maiorNumero}`)
+}
+devolveMaiorNumero(numeros)
