6f12934cb53157a51a1d82e3b6d7efcdd4c81af9
mrm28 <90999092+mrjsctwo@users.noreply.github.com>
2025-11-13 20:15:58 -0300

    Cat.ts

--- Projeto Animal/Cat.ts
+++ Projeto Animal/Cat.ts
@@ -1,1 +1,13 @@
+import {Animal} from "./Animal"
+export class Cat extends Animal{
+    
+      constructor(name: string, weight:number){
+      super(name, weight)
+   }
+      bark():void{
+          console.log('Miau,Miau')
+
+      }
+}
+
 
