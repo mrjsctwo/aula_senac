d986ab57cd13f5ae2737f3d111c24f2ab23d80ae
mrm28 <90999092+mrjsctwo@users.noreply.github.com>
2025-11-13 11:37:19 -0300

    Dog.ts

--- Dog.ts
+++ Dog.ts
@@ -1,15 +1,14 @@
 import {Animal} from "./Animal"
 export class Dog extends Animal {
     
+        constructor(name: string, weight:number){
+        super(name, weight)
+    
+  }
 
-constructor(name: string, weight:number){
-    super(name, weight)
-
-
+         bark():void{
+         console.log('Au,Au')
+      }
 }
 
-bark():void{
-    console.log('Au,Au')
- }
-}
 
