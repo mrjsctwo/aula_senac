509083528d33fe989c94747503321f738b9a9f2f
mrm28 <90999092+mrjsctwo@users.noreply.github.com>
2025-11-13 20:15:14 -0300

    Owl.ts

--- Projeto Animal/Owl.ts
+++ Projeto Animal/Owl.ts
@@ -1,1 +1,12 @@
+import {Animal} from "./Animal"
+export class Owl extends Animal{
+        
+        constructor(name: string, weight:number){
+        super(name, weight)
+     }
 
+        bark():void{
+           console.log('Wru,Wru')
+   }
+
+}
