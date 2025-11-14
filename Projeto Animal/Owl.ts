48e972558fb66cd23b67faaaf088e12ef37705a8
Murillo <90999092+mrso4res@users.noreply.github.com>
2025-11-14 15:29:21 -0300

    Owl.ts

--- Projeto Animal/Owl.ts
+++ Projeto Animal/Owl.ts
@@ -1,10 +1,12 @@
-import {Animal} from "./Animal"
-export class Owl extends Animal{
+import {Animal} from "./Animal"//Import
+export class Owl extends Animal{//Export
         
+        //Características 
         constructor(name: string, weight:number){
         super(name, weight)
      }
-
+        
+        //Latir
         bark():void{
            console.log('Wru,Wru')
    }
