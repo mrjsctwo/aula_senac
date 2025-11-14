4c762b5de01fa2e318d37d6854a43d7d2cc575ae
Murillo <90999092+mrso4res@users.noreply.github.com>
2025-11-14 15:26:56 -0300

    Cat.ts

--- Projeto Animal/Cat.ts
+++ Projeto Animal/Cat.ts
@@ -1,9 +1,11 @@
-import {Animal} from "./Animal"
-export class Cat extends Animal{
-    
+import {Animal} from "./Animal"//Importar 
+export class Cat extends Animal{//Exportar
+      
+      //Caracteristicas
       constructor(name: string, weight:number){
       super(name, weight)
    }
+      //Latir 
       bark():void{
           console.log('Miau,Miau')
 
