673886413f26d803809123b1a689e4e207090e14
Murillo <90999092+mrso4res@users.noreply.github.com>
2025-12-01 19:21:55 -0300

    Monstro.ts

--- Projeto Personagem /Monstro.ts
+++ Projeto Personagem /Monstro.ts
@@ -1,1 +1,23 @@
+class Monstro //Classe Monstro
+{
+    //1. Objeto | Caracteristicas:
+    nome:string
+    tipo:string
+    forca:number
+    saude:number
 
+    //2. Constructor | Metodo para Inicializar o Objeto:
+    constructor(monstroNome: string, monstroTipo: string, monstroForca: number)
+    {
+        this.nome = monstroNome
+        this.tipo = monstroTipo
+        this.forca = 87
+        this.saude = 56    
+    }
+
+    //3. Metodo para Chamar o Objeto:
+    atacar(guerreiro: Guerreiro): void
+    {
+        console.log(`O ${this.nome} ataca o ${guerreiro.nome}`)
+    }
+}
